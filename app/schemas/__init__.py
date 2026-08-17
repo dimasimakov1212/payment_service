b7dfa78f25e1b49521a3740e58805fffb53e21de
DimaSimakov <dimarodeo@gmail.com>
2026-08-17 10:54:04 +0000

    Create schemas for creating and getting payments

---
+++ app/schemas/__init__.py
@@ -0,0 +1,7 @@
+from app.schemas.payment import (
+    PaymentCreateRequest,
+    PaymentCreateResponse,
+    PaymentResponse,
+)
+
+__all__ = ("PaymentCreateRequest", "PaymentCreateResponse", "PaymentResponse")
